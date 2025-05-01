4885614bcbebcaa42ec441ef65126ec6fad9540e
Jairo Allende <jairoalle23@gmail.com>
2025-05-01 09:58:18 -0300

    Se elimino la clase(antigua) Vehiculo

--- src/ar/edu/unlam/dominio/Vehiculo.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.unlam.dominio;
-
-public interface Vehiculo {
-	boolean agregarPaquete(Paquete paquete, String ciudadDeDestino);
-	boolean realizarEntrega(Paquete paquete);
-}
--- test/ar/edu/unlam/dominio/PasajeroTest.java
+++ test/ar/edu/unlam/dominio/PasajeroTest.java
@@ -29,4 +29,6 @@ public class PasajeroTest {
 		
 		assertEquals(pesoPasajeroEsperado, pesoPasajero1);
 	}
+	
+	
 }
